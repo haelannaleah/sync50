537f877dc8688f967151425ef2ca6ba107c1aa14
Annaleah Elizabeth Ernst <haelannaleah@gmail.com>
2016-01-15 22:47:07 +0000

    begin work on file monitering daemon

--- sync50.sh
+++ sync50.sh
@@ -6,6 +6,10 @@
 # jterm 2016
 # annaleahernst@college.harvard.edu
 
+############### BEGIN GLOBAL AND CONSTANT DECLARATIONS #########################
+
+set -uo pipefail
+IFS=$'\n\t'
 
 # important cloud paths
 readonly CLDWKSPCS="workspaces"
@@ -17,6 +21,7 @@ readonly WORKDIR="${HOME}/workspace/bash_prac"   # TODO: address to workspace
 readonly COPYLOC="${HOME}/copy"                  # location of the copy folder
 readonly DOTCOPY="${HOME}/.copy"                 # location of the metadata
 readonly STATUSFILE="$DOTCOPY/status.txt"        # location of status data
+readonly WATCHPID="$DOTCOPY/watchPID.txt"       # location of process id for running watch daemon
 readonly SYNCDIR="$DOTCOPY/userdata"             # root directory of copy.com data
 readonly SYMLINKDIR="$SYNCDIR/$CLDUSERS"         # directory containing symlink
 readonly SYMLINK="$SYMLINKDIR/${C9_PROJECT}"     # symlink to workspace
@@ -30,6 +35,7 @@ STATUS=""
 readonly INVALIDLOGN="Incorrect username or password"
 readonly MISSLOGIN="Missing Login Information"
 readonly NOTINSTALLED="Sync50 has not been installed. Type 'sync50 start' to begin setup."
+readonly NOWATCH="No watch running"
 
 # process variables; name, ID
 readonly PROCESSNAME="CopyConsole"
@@ -46,27 +52,60 @@ readonly STOPFAILURE="Unable to stop sync50 at this time."
 readonly EXREGEX='s/^\///gm'                                # for listing excludes
 readonly LSREGEX='s/^d.[ \t\f\v]*|^-.[ \t\f\v]*(\S* )//gm'  # for listing files
 
+################## BEGIN FUNCTION DEFINITIONS ##################################
+
+# climb through file tree and exlude all files not in project to prevent bricking IDE
+# usage: exclude [FILE/FOLDER TO KEEP] [PATH TO CURRENT DIR]
+exclude(){
+    exclude_helper(){
+        dir=""
+        path=""
+        # get stripped down file/directory names
+        if [ $# = 2 ]; then
+            # starting at 3rd line of search results; first line is metadata, 2nd is pwd
+            dir=$($COPYCMD Cloud ls "$2" | sed -r "$LSREGEX" | tail -n +3)
+            path="$2/"
+        else
+            # starting at 2nd line of search results; first is metadata
+            dir=$($COPYCMD Cloud ls | sed -r "$LSREGEX" | tail -n +2)
+        fi
+        echo "$dir" | while read file; do
+            if [ "$file" != "$1" ]; then
+                echo -n "... "
+                $COPYCMD Cloud exclude -exclude "$path$file" > /dev/null
+            fi
+        done
+    }
+    # exclude files in each sub directory of our workspaces tree
+    exclude_helper "$CLDWKSPCS"
+    exclude_helper "${C9_USER}" "$CLDWKSPCS"
+    exclude_helper "${C9_PROJECT}" "$CLDUSERS"
+    echo
+}
+
 # displays information on the usage of this wrapper
 help(){
     echo "Usage: sync50 [FUNCTION]"
     echo "Functions:"
     echo -e "\tstart\t\tstart copy.com syncing, or install if first run"
     echo -e "\tstop\t\tstop Copy.com syncing"
-    echo -e "\tstatus\t\tget the status of the current instance"
     echo -e "\tuninstall\t\tremove Copy.com files"
 }
 
 # installs the copy.com binaries for the first time
 install(){
     if [ ! -e "$COPYCMD" ]; then
-        echo "Downloading Copy...."
-        wget -q -O - "https://copy.com/install/linux/Copy.tgz" |  tar xzf -
+        echo "Downloading Copy...."; echo -n "... "
+        wget -q -O - "https://copy.com/install/linux/Copy.tgz" |  tar xzf - > /dev/null; echo -n "... "
+        sudo apt-get install inotify-tools > /dev/null
+        echo -n "..."; echo
         
         # make the metadata folder if copy.com was lazy
         if [ ! -e "$STATUSFILE" ]; then
             mkdir -p "$DOTCOPY"
-            echo "$MISSLOGIN" > "$STATUSFILE"
+            echo -n "$MISSLOGIN" > "$STATUSFILE"
         fi
+        echo "$NOWATCH" > "$WATCHPID"
         
         # create root dir for the cloud and link to current workspace 
         mkdir -p "$SYMLINKDIR"  
@@ -77,29 +116,6 @@ install(){
         echo "Already installed."
     fi
     start
-}
-
-
-# climb through file tree and exlude all files not in project to prevent bricking IDE
-# usage: exclude [FILE/FOLDER TO KEEP] [PATH TO CURRENT DIR]
-exclude(){
-    dir=""
-    path=""
-    # get stripped down file/directory names
-    if [ $# = 2 ]; then
-        # starting at 3rd line of search results; first line is metadata, 2nd is pwd
-        dir=$($COPYCMD Cloud ls "$2" | sed -r "$LSREGEX" | tail -n +3)
-        path="$2/"
-    else
-        # starting at 2nd line of search results; first is metadata
-        dir=$($COPYCMD Cloud ls | sed -r "$LSREGEX" | tail -n +2)
-    fi
-    echo "$dir" | while read file; do
-        echo -n "... "
-        if [ "$file" != "$1" ]; then
-            $COPYCMD Cloud exclude -exclude "$path$file" > /dev/null
-        fi
-    done
 }
 
 # log in to copy.com after install
@@ -113,11 +129,11 @@ login(){
         [ -z $pswrd ] && echo "$INVALIDLOGN" && exit    # check for null password
         
         # attempt to login user
-        $COPYCONSOLE -daemon -u="$email" -p="$pswrd" -r="$SYNCDIR" > /dev/null
-        #$COPYCONSOLE -daemon -u="annaleahernst@college.harvard.edu" -p="abc123ABC" -r="$SYNCDIR" > /dev/null
+        #$COPYCONSOLE -daemon -u="$email" -p="$pswrd" -r="$SYNCDIR" > /dev/null
+        $COPYCONSOLE -daemon -u="annaleahernst@college.harvard.edu" -p="abc123ABC" -r="$SYNCDIR" > /dev/null
         process "init"
     else
-        echo "Before proceeding with setup, you need a copy.com account."
+        echo "Before completing setup, you need a copy.com account."
         echo "Please proceed to https://www.copy.com and create an account. Exiting..."
     fi
 }
@@ -130,28 +146,28 @@ login(){
 #       immediately run stop to prevent syncing. Run exlcude on the cloud file tree. Finally,
 #       run start to begin syncing.
 process(){
-    CURRENTPID=$PID
-    COUNTER=0
+    currentPID=$PID
+    counter=0
     # until there's a process id state change or we run out of time, search for process
-    until [ "$CURRENTPID" != "$PID" ] || [ $COUNTER = $TIMEOUT ]; do
+    until [ "$currentPID" != "$PID" ] || [ $counter = $TIMEOUT ]; do
         echo -n "... "
         sleep 1
-        CURRENTPID=`ps -ef | grep "$PROCESSNAME" | grep -v grep | awk '{print $2}'`
-        let COUNTER+=1
+        currentPID=$(ps -ef | grep "$PROCESSNAME" | grep -v grep | awk '{print $2}')
+        let counter+=1
     done; echo
-    PID="$CURRENTPID" # update current PID
+    PID="$currentPID" # update current PID
     
     case "$1" in
         "start")
             # if there's a process, echo succcess, else failure
-            [[ -n "$CURRENTPID" ]] && echo "$STARTSUCCESS" || echo "$STARTFAILURE"
+            [[ -n "$currentPID" ]] && echo "$STARTSUCCESS" || echo "$STARTFAILURE"
             ;;
         "stop")
             # if there's not a process, echo succcess, else failure
-            [[ -z "$CURRENTPID" ]] && echo "$STOPSUCCESS" || echo "$STOPFAILURE"
+            [[ -z "$currentPID" ]] && echo "$STOPSUCCESS" || echo "$STOPFAILURE"
             ;;
         "init")
-            if [ -z "$CURRENTPID" ]; then
+            if [ -z "$currentPID" ]; then
                 # if there's not a process running, there was an error
                 status
                 echo "$STATUS. Exiting..." 
@@ -159,13 +175,8 @@ process(){
             else
                 # stop syncing and handle excludes
                 stop > /dev/null
-                echo "Setup successful. Beginning excludes. This may take a few minutes..."
-                
-                # exclude files in each sub directory of our workspaces tree
-                exclude "$CLDWKSPCS"
-                exclude "${C9_USER}" "$CLDWKSPCS"
-                exclude "${C9_PROJECT}" "$CLDUSERS"
-                echo
+                echo "Setup successful. Beginning exclusions. This may take a few minutes..."
+                exclude
                 echo "Exclusions complete."
                 start
             fi
@@ -194,7 +205,13 @@ start(){
         login
     elif [ -z "$PID" ]; then
         echo "Starting..."
-        $COPYCONSOLE -daemon > /dev/null    # start copy daemon
+        
+        # begin watching directories of interest for unwanted changes and store process id
+        inotifywait "@$SYNCDIR/$CLDPRJCT" "$SYNCDIR" "$SYNCDIR/$CLDWKSPCS" "$SYNCDIR/$CLDUSERS" \
+            -e move,create & echo $! > "$WATCHPID"
+            
+        # start copy daemon
+        $COPYCONSOLE -daemon > /dev/null    
         process "start"
     else
         echo "Sync50 is already running!"
@@ -205,6 +222,7 @@ start(){
 stop(){
     if [ -n "$PID" ]; then
         echo "Stopping..."
+        kill $(head -n 1 $WATCHPID); echo "$NOWATCH" > "$WATCHPID" # remove watch
         kill $PID
         process "stop"
     else
@@ -217,17 +235,20 @@ uninstall(){
     if [ -e $COPYCMD ]; then
         echo -n "Are you sure you want to uninstall Copy? [y/N]: "; read prompt
         if [[ "$prompt" =~ [yY](es)* ]]; then 
-            echo "Uninstalling..."
+            echo "Uninstalling..."; echo -n "... "
             stop > /dev/null
-            unlink "$SYMLINK"
-            rm -rf "$COPYLOC"
-            rm -rf "$DOTCOPY"
+            unlink "$SYMLINK"; echo -n "... "
+            rm -rf "$COPYLOC"; echo -n "... "
+            rm -rf "$DOTCOPY"; echo
             echo "Copy has been uninstalled."
         fi
     else 
         echo "Copy is not installed!"
     fi
 }
+
+
+######################### BEGIN BODY OF PROGRAM ##############################
 
 pushd ${HOME} >/dev/null
 
@@ -260,8 +281,6 @@ case "$1" in
         ;;
 esac
 exit
-
-# remove the login line, and all non file name lines
 
 # cloudFiles=`$COPYCMD Cloud ls | sed -r 's/^d.[ \t\f\v]*|^-.[ \t\f\v]*(\S* )//gm' | tail -n +2`
 
